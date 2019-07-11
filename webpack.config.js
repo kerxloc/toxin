a5ed715851551d9621a2c412a55bb326ab83096e
dywork <dubrovskywork@gmail.com>
2019-07-11 19:39:20 +0500

    добавил gh-pages для деплоя приложения

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
+    publicPath: "/toxin/"
   },
   devServer: {
     port: 3000,
