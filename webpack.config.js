8f6fd08450a670c731cbee8c257202900f66566a
dywork <dubrovskywork@gmail.com>
2019-07-11 13:25:47 +0500

    first initial

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  devServer: {
+    port: 3000,
+    clientLogLevel: "silent"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.pug"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: ["pug-loader"]
+      }
+    ]
+  }
+};
