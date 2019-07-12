f960469dad53fb2bca88e63c02df524d320b23bb
dywork <dubrovskywork@gmail.com>
2019-07-12 17:05:09 +0500

    prettier refact

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,18 @@
 module.exports = api => {
-  const isProd = api.cache(() => process.env.NODE_ENV === 'production')
+  const isProd = api.cache(() => process.env.NODE_ENV === 'production');
 
   return {
-    plugins: [
-      '@babel/plugin-syntax-dynamic-import'
-    ],
+    plugins: ['@babel/plugin-syntax-dynamic-import'],
     presets: [
       [
         '@babel/preset-env',
         {
           modules: false,
           targets: {
-            esmodules: !isProd
-          }
-        }
-      ]
-    ]
-  }
-}
+            esmodules: !isProd,
+          },
+        },
+      ],
+    ],
+  };
+};
