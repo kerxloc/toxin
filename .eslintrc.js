b7578d296c1779a2f6ef7c3a2568e43141d8fe9f
dywork <dubrovskywork@gmail.com>
2019-07-12 17:04:45 +0500

    update linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,58 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
     es6: true,
-    node: true
+    browser: true,
+    node: true,
   },
-  plugins: ['import', 'promise', 'compat', 'node'],
-  extends: [
-    'plugin:promise/recommended',
-    'standard'
-  ],
+  extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+  plugins: ['babel', 'import', 'jsx-a11y', 'react', 'prettier'],
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 6,
     sourceType: 'module',
-    ecmaVersion: 9,
     ecmaFeatures: {
-      sourceType: 'module',
-      jsx: true
+      jsx: true,
     },
-    allowImportExportEverywhere: true
   },
   rules: {
-    'promise/always-return': 0,
-    'promise/avoid-new': 0,
-    'compat/compat': 1,
-    'node/no-deprecated-api': 2,
-    'node/no-extraneous-require': 2,
-    'node/no-missing-require': 2,
-    'import/no-unresolved': [2, { commonjs: true, amd: true }],
-    'import/named': 2,
-    'import/namespace': 2,
-    'import/default': 2,
-    'import/export': 2,
-    'no-console': 1,
-    'curly': 0
-  }
-}
+    'linebreak-style': 'off',
+
+    'arrow-parens': 'off',
+    'object-curly-newline': 'off',
+    'no-mixed-operators': 'off',
+    'arrow-body-style': 'off',
+    'function-paren-newline': 'off',
+    'no-plusplus': 'off',
+    'space-before-function-paren': 0,
+
+    'max-len': ['error', 100, 2, { ignoreUrls: true }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-alert': 'error',
+
+    'no-param-reassign': 'off',
+    radix: 'off',
+
+    'react/require-default-props': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js'] }],
+
+    'prefer-destructuring': 'off',
+
+    'react/no-find-dom-node': 'off',
+    'react/no-did-mount-set-state': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+
+    'jsx-a11y/anchor-is-valid': ['error', { components: ['Link'], specialLink: ['to'] }],
+    'jsx-a11y/label-has-for': [
+      2,
+      {
+        required: {
+          every: ['id'],
+        },
+      },
+    ],
+
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
