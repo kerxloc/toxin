fabe0c156321d63f800a6dd1e620756597fdb1c4
dywork <dubrovskywork@gmail.com>
2019-07-26 15:42:21 +0500

    описал рендер DatePicker

---
+++ app/components/date-picker/date-picker.js
@@ -0,0 +1,109 @@
+function getHtmlElement(tagName, className, text) {
+  const element = document.createElement(tagName);
+
+  if (className) {
+    element.classList.add(className);
+  }
+
+  if (text) {
+    element.textContent = text;
+  }
+
+  return element;
+}
+
+class DatePicker {
+  getCalendarTopControl = () => {
+    const datePickerHtmlControl = getHtmlElement('div', 'date-picker__control');
+    const datePickerHtmlSliderBtnPrev = getHtmlElement(
+      'button',
+      'date-picker__slider-btn',
+      'Назад',
+    );
+    datePickerHtmlSliderBtnPrev.classList.add('date-picker__slider-btn--prev');
+
+    const datePickerHtmlSliderBtnNext = getHtmlElement(
+      'button',
+      'date-picker__slider-btn',
+      'Вперед',
+    );
+    datePickerHtmlSliderBtnNext.classList.add('date-picker__slider-btn--next');
+
+    const datePickerHtmlTitle = getHtmlElement('h2', 'date-picker__title', 'Август 2019');
+
+    datePickerHtmlControl.appendChild(datePickerHtmlSliderBtnPrev);
+    datePickerHtmlControl.appendChild(datePickerHtmlTitle);
+    datePickerHtmlControl.appendChild(datePickerHtmlSliderBtnNext);
+
+    return datePickerHtmlControl;
+  };
+
+  getCalendarTable = () => {
+    const calendarTable = getHtmlElement('table', 'date-picker__calendar');
+    const tBody = getHtmlElement('tbody');
+    const tableTrHead = getHtmlElement('tr');
+    const tableHead = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+    tableHead.forEach(item => {
+      const th = getHtmlElement('th', 'date-picker__th', item);
+      tableTrHead.appendChild(th);
+    });
+
+    tBody.appendChild(tableTrHead);
+
+    for (let i = 0; i < 5; i++) {
+      const tableTr = getHtmlElement('tr');
+
+      for (let j = 0; j < 7; j++) {
+        const tableTd = getHtmlElement('td', 'date-picker__day', j + 1);
+        tableTr.appendChild(tableTd);
+      }
+
+      tBody.appendChild(tableTr);
+    }
+
+    calendarTable.appendChild(tBody);
+    return calendarTable;
+  };
+
+  getCalendarBotControl = () => {
+    const datePickerHtmlControl = getHtmlElement('div', 'date-picker__control');
+    const datePickerButtons = [
+      { text: 'Очистить', isAccent: false },
+      { text: 'Применить', isAccent: true },
+    ];
+
+    datePickerButtons.forEach(item => {
+      const btn = getHtmlElement('button', 'date-picker__button', item.text);
+      btn.type = 'button';
+
+      if (item.isAccent) {
+        btn.classList.add('date-picker__button--accent');
+      }
+
+      datePickerHtmlControl.appendChild(btn);
+    });
+    return datePickerHtmlControl;
+  };
+
+  getCalendar = () => {
+    const datePickerHtmlSection = getHtmlElement('section', 'date-picker');
+    const datePickerHtmlWrap = getHtmlElement('div', 'date-picker__wrap');
+    const datePickerHtmlControl = this.getCalendarTopControl();
+    const datePickerHtmlTable = this.getCalendarTable();
+    const datePickerHtmlBotControl = this.getCalendarBotControl();
+    datePickerHtmlWrap.appendChild(datePickerHtmlControl);
+    datePickerHtmlWrap.appendChild(datePickerHtmlTable);
+    datePickerHtmlWrap.appendChild(datePickerHtmlBotControl);
+    datePickerHtmlSection.appendChild(datePickerHtmlWrap);
+
+    return datePickerHtmlSection;
+  };
+
+  renderCalendar = (parentNode = document.body) => {
+    const calendar = this.getCalendar();
+    parentNode.appendChild(calendar);
+  };
+}
+
+export default DatePicker;
