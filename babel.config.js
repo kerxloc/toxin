2f8a71fa1c317a904fc6f1ae9b14a216e9cf3046
dywork <dubrovskywork@gmail.com>
2019-07-26 15:41:27 +0500

    добавил поддержку proposal-class-properties в babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,10 @@ module.exports = api => {
   const isProd = api.cache(() => process.env.NODE_ENV === 'production');
 
   return {
-    plugins: ['@babel/plugin-syntax-dynamic-import'],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
     presets: [
       [
         '@babel/preset-env',
