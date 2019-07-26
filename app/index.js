3325b61e4b6d01a609174f254a7835c0f88f775f
dywork <dubrovskywork@gmail.com>
2019-07-26 15:42:09 +0500

    проинициализировал DatePicker

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 /* eslint-disable global-require */
 import './styles/main.scss';
+import DatePicker from './components/date-picker/date-picker';
 
 if (process.env.NODE_ENV !== 'production') {
   require('./index.pug');
   require('./pages/about.pug');
 }
+
+const datePickerContainer = document.querySelector('#date-picker-container');
+const datePicker = new DatePicker();
+datePicker.renderCalendar(datePickerContainer);
