1aae9852d0f7146f303c5603e4d540f2f3146578
dywork <dubrovskywork@gmail.com>
2019-09-20 10:18:32 +0500

    разметил drop-down

--- app/components/drop-down/drop-down.js
+++ app/components/drop-down/drop-down.js
@@ -25,11 +25,50 @@ class DropDown {
     } else {
       console.error('Expected input(node) inside constructor object but not received');
     }
+
+    if (options.countElements) {
+      this.countElements = options.countElements;
+    } else {
+      console.error('Expected countElements(Array) inside constructor object but not received');
+    }
   }
 
   init = () => {
-    const dropDownHtml = getHtmlElement('section', 'drop-down', 'Hello world!');
-    this.container.appendChild(dropDownHtml);
+    const dropDownParent = getHtmlElement('section', 'drop-down');
+    const dropDownParentWrap = getHtmlElement('div', 'drop-down__wrap');
+    const countList = getHtmlElement('ul', 'drop-down__count-list');
+    const dropDownControl = getHtmlElement('div', 'drop-down__control');
+    const clearBtn = getHtmlElement('button', 'drop-down__button', 'Очистить');
+    clearBtn.type = 'button';
+    const acceptBtn = getHtmlElement('button', 'drop-down__button', 'Применить');
+    acceptBtn.type = 'button';
+    acceptBtn.classList.add('drop-down__button--accent');
+    const countListFragment = document.createDocumentFragment();
+    this.countElements.forEach(element => {
+      const countItem = getHtmlElement('li', 'drop-down__count-item');
+      const countItemName = getHtmlElement('p', 'drop-down__count-item-name', element.name);
+      const counterMenu = getHtmlElement('div', 'drop-down__counter-menu');
+      const countItemMinus = getHtmlElement('button', 'drop-down__counter-btn');
+      countItemMinus.classList.add('drop-down__counter-btn--plus');
+      const countItemView = getHtmlElement('p', 'drop-down__select-view', '0');
+      const countItemPlus = getHtmlElement('button', 'drop-down__counter-btn');
+      countItemPlus.classList.add('drop-down__counter-btn--minus');
+      countItemMinus.type = 'button';
+      countItemPlus.type = 'button';
+      counterMenu.appendChild(countItemPlus);
+      counterMenu.appendChild(countItemView);
+      counterMenu.appendChild(countItemMinus);
+      countItem.appendChild(countItemName);
+      countItem.appendChild(counterMenu);
+      countListFragment.appendChild(countItem);
+    });
+    countList.appendChild(countListFragment);
+    dropDownControl.appendChild(clearBtn);
+    dropDownControl.appendChild(acceptBtn);
+    dropDownParentWrap.appendChild(countList);
+    dropDownParentWrap.appendChild(dropDownControl);
+    dropDownParent.appendChild(dropDownParentWrap);
+    this.container.appendChild(dropDownParent);
   };
 }
 
