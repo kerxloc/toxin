c02fc747c4f4fbe2bc6a98c606b89d8f3a710c2f
dywork <dubrovskywork@gmail.com>
2019-10-17 12:03:02 +0500

    реализовал механизм скрытия и появления глав. меню и проф. меню

--- app/components/main-menu/main-menu.js
+++ app/components/main-menu/main-menu.js
@@ -1,6 +1,27 @@
 const burgerButton = document.querySelector('#mainMenuBurgerIcon');
+const profileButton = document.querySelector('#authProfile');
+const mainMenu = document.querySelector('.main-menu');
+const authList = document.querySelector('.page-header__auth-list');
+let isProfileActive = false;
 
 burgerButton.addEventListener('click', evt => {
   evt.preventDefault();
+  if (isProfileActive) {
+    authList.classList.remove('page-header__auth-list--active');
+    isProfileActive = false;
+  } else {
+    mainMenu.classList.toggle('main-menu--active');
+  }
   burgerButton.classList.toggle('main-menu__burger-icon--active');
+  profileButton.classList.toggle('page-header__auth-profile--hide');
+});
+
+profileButton.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (!isProfileActive) {
+    authList.classList.add('page-header__auth-list--active');
+    burgerButton.classList.add('main-menu__burger-icon--active');
+    isProfileActive = true;
+    profileButton.classList.add('page-header__auth-profile--hide');
+  }
 });
