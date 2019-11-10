e7a84b212e661475488b65c03ad55698d54aee4d
dywork <dubrovskywork@gmail.com>
2019-11-10 14:12:30 +0500

    использую browserlist в package

--- webpack.config.js
+++ webpack.config.js
@@ -45,9 +45,7 @@ module.exports = {
             loader: "postcss-loader",
             options: {
               plugins: [
-                autoprefixer({
-                  browsers: ["ie >= 8", "last 4 version"]
-                })
+                autoprefixer()
               ]
             }
           },
