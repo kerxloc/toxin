7c607a13ebb9db6a38939999184663f77e963981
dywork <dubrovskywork@gmail.com>
2019-11-11 20:29:18 +0500

    подключил маски для дейт дропдаунов

--- src/form-elements.js
+++ src/form-elements.js
@@ -4,3 +4,9 @@ import Inputmask from "inputmask";
 Inputmask({ mask: "99.99.9999", placeholder: "ДД.ММ.ГГГГ" }).mask(
   "#maskedTextField"
 );
+Inputmask({ mask: "99.99.9999", placeholder: "ДД.ММ.ГГГГ" }).mask(
+  "#arrival-input"
+);
+Inputmask({ mask: "99.99.9999", placeholder: "19.08.2019" }).mask(
+  "#departure-input"
+);
