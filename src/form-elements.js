09e609d328ea218aa3a3beba381fddc2a790fb6d
dywork <dubrovskywork@gmail.com>
2019-11-19 16:31:13 +0500

    добавил инициализацию дропдауна для form-elements

--- src/form-elements.js
+++ src/form-elements.js
@@ -1,5 +1,6 @@
 import "./style/main.scss";
 import Inputmask from "inputmask";
+import DropDown from "./components/drop-down/drop-down";
 
 Inputmask({ mask: "99.99.9999", placeholder: "ДД.ММ.ГГГГ" }).mask(
   "#maskedTextField"
@@ -10,3 +11,25 @@ Inputmask({ mask: "99.99.9999", placeholder: "ДД.ММ.ГГГГ" }).mask(
 Inputmask({ mask: "99.99.9999", placeholder: "19.08.2019" }).mask(
   "#departure-input"
 );
+
+const dropDownContainer = document.querySelector("#drop-down-container");
+const inputDropDown = document.querySelector("#number-guests-input2");
+
+const dropDownOptions = {
+  container: dropDownContainer,
+  input: inputDropDown,
+  countElements: [
+    { name: "Спальни", countGroupName: "bedrooms" },
+    { name: "Кровати", countGroupName: "bed" },
+    { name: "Ванные комнаты", countGroupName: "bath" }
+  ],
+  countGroupView: {
+    bedrooms: { counter: 2, views: ["спальня", "спальни", "спален"] },
+    bed: { counter: 2, views: ["кровать", "кровати", "кроватей"] },
+    bath: { counter: 1, views: ["ванная комната", "ванные комнаты", "ванных комнат"] }
+  },
+  placeholder: "Удобства номера"
+};
+
+const dropDown = new DropDown(dropDownOptions);
+dropDown.init();
