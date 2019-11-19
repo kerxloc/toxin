fa9da89775571ab9051ace17d373b1f1d50cd4d2
dywork <dubrovskywork@gmail.com>
2019-11-19 18:55:48 +0500

    Добавил значение startvalue для настроек dropDown

--- src/form-elements.js
+++ src/form-elements.js
@@ -19,15 +19,15 @@ const dropDownOptions = {
   container: dropDownContainer,
   input: inputDropDown,
   countElements: [
-    { name: "Спальни", countGroupName: "bedrooms" },
-    { name: "Кровати", countGroupName: "bed" },
-    { name: "Ванные комнаты", countGroupName: "bath" }
+    { name: "Спальни", countGroupName: "bedrooms", startValue: 2 },
+    { name: "Кровати", countGroupName: "bed", startValue: 2 },
+    { name: "Ванные комнаты", countGroupName: "bath", startValue: 0 }
   ],
   countGroupView: {
     bedrooms: { counter: 2, views: ["спальня", "спальни", "спален"] },
     bed: { counter: 2, views: ["кровать", "кровати", "кроватей"] },
     bath: {
-      counter: 1,
+      counter: 0,
       views: ["ванная комната", "ванные комнаты", "ванных комнат"]
     }
   },
