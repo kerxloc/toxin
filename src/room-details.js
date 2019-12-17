aa53718ad33ac79a2b4ebe268c0fbeeda309a7db
dywork <dubrovskywork@gmail.com>
2019-12-18 01:13:45 +0500

    добавил градиент в донат диаграму

--- src/room-details.js
+++ src/room-details.js
@@ -1,6 +1,20 @@
 import "./style/main.scss";
 import Chart from "chart.js";
 
+const ctx = document.getElementById("doughnut-chart").getContext("2d");
+
+const yellowGradient = ctx.createLinearGradient(0, 0, 0, 120);
+yellowGradient.addColorStop(0, "#FFE39C");
+yellowGradient.addColorStop(1, "#FFBA9C");
+
+const purpleGradient = ctx.createLinearGradient(0, 0, 0, 60);
+purpleGradient.addColorStop(0, "#BC9CFF");
+purpleGradient.addColorStop(1, "#8BA4F9");
+
+const greenGradient = ctx.createLinearGradient(0, 60, 0, 120);
+greenGradient.addColorStop(0, "#6FCF97");
+greenGradient.addColorStop(1, "#66D2EA");
+
 new Chart(document.getElementById("doughnut-chart"), {
   type: "doughnut",
   data: {
@@ -9,7 +23,7 @@ new Chart(document.getElementById("doughnut-chart"), {
         data: [25, 25, 50],
         borderWidth: 3,
         hoverBorderWidth: 0,
-        backgroundColor: ["#bc9cff", "#6fcf97", "#ffe39c"]
+        backgroundColor: [purpleGradient, greenGradient, yellowGradient]
       }
     ]
   },
@@ -23,5 +37,5 @@ new Chart(document.getElementById("doughnut-chart"), {
     tooltips: {
       enabled: false
     }
-  } 
+  }
 });
