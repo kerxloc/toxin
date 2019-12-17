b9b6ad1f2c275f1ac7b5e6f8974e0abeba711306
dywork <dubrovskywork@gmail.com>
2019-12-18 00:54:59 +0500

    описал круговую диаграму с помощью Chart.js

--- src/room-details.js
+++ src/room-details.js
@@ -1,1 +1,27 @@
 import "./style/main.scss";
+import Chart from "chart.js";
+
+new Chart(document.getElementById("doughnut-chart"), {
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        data: [25, 25, 50],
+        borderWidth: 3,
+        hoverBorderWidth: 0,
+        backgroundColor: ["#bc9cff", "#6fcf97", "#ffe39c"]
+      }
+    ]
+  },
+  options: {
+    animation: {
+      animateScale: false,
+      animateRotate: false
+    },
+    cutoutPercentage: 85,
+    legend: false,
+    tooltips: {
+      enabled: false
+    }
+  } 
+});
