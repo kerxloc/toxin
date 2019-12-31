5704059e1268f761ec87d16824377bbe15c34b64
dywork <dubrovskywork@gmail.com>
2019-12-31 18:59:25 +0500

    переписал main-menu на классы для возможности использования на 1 страницы нескольких меню

--- src/components/main-menu/main-menu.js
+++ src/components/main-menu/main-menu.js
@@ -1,27 +1,58 @@
-const burgerButton = document.querySelector("#mainMenuBurgerIcon");
-const profileButton = document.querySelector("#authProfile");
-const mainMenu = document.querySelector(".main-menu");
-const authList = document.querySelector(".page-header__auth-list");
-let isProfileActive = false;
+class MainMenu {
+  constructor(options) {
+    if (options.mainMenu) {
+      this.mainMenu = options.mainMenu;
+    } else {
+      console.error(
+        "Expected burgerButton(node) inside constructor object but not received"
+      );
+    }
 
-burgerButton.addEventListener("click", evt => {
-  evt.preventDefault();
-  if (isProfileActive) {
-    authList.classList.remove("page-header__auth-list--active");
-    isProfileActive = false;
-  } else {
-    mainMenu.classList.toggle("main-menu--active");
-  }
-  burgerButton.classList.toggle("main-menu__burger-icon--active");
-  profileButton.classList.toggle("page-header__auth-profile--hide");
-});
+    if (options.burgerButton) {
+      this.burgerButton = options.burgerButton;
+      this.burgerButton.addEventListener("click", this.onBurgerButtonClick);
+    } else {
+      console.error(
+        "Expected burgerButton(node) inside constructor object but not received"
+      );
+    }
 
-profileButton.addEventListener("click", evt => {
-  evt.preventDefault();
-  if (!isProfileActive) {
-    authList.classList.add("page-header__auth-list--active");
-    burgerButton.classList.add("main-menu__burger-icon--active");
-    isProfileActive = true;
-    profileButton.classList.add("page-header__auth-profile--hide");
+    if (options.profileButton) {
+      this.profileButton = options.profileButton;
+      this.profileButton.addEventListener("click", this.onProfileButtonClick);
+    } else {
+      this.profileButton = undefined;
+    }
+
+    this.isProfileActive = false;
+    this.authList = options.authList;
   }
-});
+
+  onBurgerButtonClick = evt => {
+    evt.preventDefault();
+
+    if (this.isProfileActive) {
+      this.authList.classList.remove("page-header__auth-list--active");
+      this.isProfileActive = false;
+    } else {
+      this.mainMenu.classList.toggle("main-menu--active");
+    }
+
+    this.burgerButton.classList.toggle("main-menu__burger-icon--active");
+    if (this.profileButton) {
+      this.profileButton.classList.toggle("page-header__auth-profile--hide");
+    }
+  };
+
+  onProfileButtonClick = evt => {
+    evt.preventDefault();
+    if (!this.isProfileActive) {
+      this.authList.classList.add("page-header__auth-list--active");
+      this.burgerButton.classList.add("main-menu__burger-icon--active");
+      this.isProfileActive = true;
+      this.profileButton.classList.add("page-header__auth-profile--hide");
+    }
+  };
+}
+
+export default MainMenu;
