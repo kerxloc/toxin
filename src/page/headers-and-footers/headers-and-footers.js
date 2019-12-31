4c99cd0c8df3d92e0a6edcf88a60e1d647d410fd
dywork <dubrovskywork@gmail.com>
2019-12-31 19:00:04 +0500

    инициализирую скрипт main-menu для страницы headers-and-footers

--- src/page/headers-and-footers/headers-and-footers.js
+++ src/page/headers-and-footers/headers-and-footers.js
@@ -1,2 +1,18 @@
 import "../../style/index.scss";
 import "./headers-and-footers.scss";
+import MainMenu from "../../components/main-menu/main-menu";
+
+const firstMainMenuHtmlNode = {
+  mainMenu: document.querySelector("#firstMainMenu"),
+  burgerButton: document.querySelector("#firstBurgerBtn"),
+  authList: document.querySelector("#firstAuthList"),
+  profileButton: document.querySelector("#firstProfileBtn")
+};
+
+const secondMainMenuHtmlNode = {
+  mainMenu: document.querySelector("#secondMainMenu"),
+  burgerButton: document.querySelector("#secondBurgerBtn")
+};
+
+new MainMenu(firstMainMenuHtmlNode);
+new MainMenu(secondMainMenuHtmlNode);
