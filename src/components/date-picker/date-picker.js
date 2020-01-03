c4c2cb830be9bed7c27b6111ca9109fb3b01e278
dywork <dubrovskywork@gmail.com>
2020-01-03 08:24:26 +0500

    удалил комментарии в функции dom

--- src/components/date-picker/date-picker.js
+++ src/components/date-picker/date-picker.js
@@ -2,13 +2,11 @@ import Inputmask from "inputmask";
 import isNumeric from "validator/lib/isNumeric";
 
 function dom(tag, attrs, ...children) {
-  // Custom Components will be functions
   if (typeof tag === "function") {
     return tag();
   }
-  // regular html tags will be strings to create the elements
+
   if (typeof tag === "string") {
-    // fragments to append multiple children to the initial node
     const fragments = document.createDocumentFragment();
     const element = document.createElement(tag);
     children.forEach(child => {
@@ -18,12 +16,11 @@ function dom(tag, attrs, ...children) {
         const textnode = document.createTextNode(child);
         fragments.appendChild(textnode);
       } else {
-        // later other things could not be HTMLElement not strings
         console.log("not appendable", child);
       }
     });
+
     element.appendChild(fragments);
-    // Merge element with attributes
     Object.assign(element, attrs);
     return element;
   }
