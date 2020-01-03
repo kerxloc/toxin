01198fdeb354843bfc897200f8b4b4c7b9542e54
dywork <dubrovskywork@gmail.com>
2020-01-03 08:44:44 +0500

    удалил Dom

--- src/components/date-picker/date-picker.js
+++ src/components/date-picker/date-picker.js
@@ -1,31 +1,6 @@
 import Inputmask from "inputmask";
 import isNumeric from "validator/lib/isNumeric";
 
-function dom(tag, attrs, ...children) {
-  if (typeof tag === "function") {
-    return tag();
-  }
-
-  if (typeof tag === "string") {
-    const fragments = document.createDocumentFragment();
-    const element = document.createElement(tag);
-    children.forEach(child => {
-      if (child instanceof HTMLElement) {
-        fragments.appendChild(child);
-      } else if (typeof child === "string") {
-        const textnode = document.createTextNode(child);
-        fragments.appendChild(textnode);
-      } else {
-        console.log("not appendable", child);
-      }
-    });
-
-    element.appendChild(fragments);
-    Object.assign(element, attrs);
-    return element;
-  }
-}
-
 const monthRusTranslate = {
   0: "Январь",
   1: "Февраль",
