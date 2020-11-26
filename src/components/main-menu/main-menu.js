3d9466b73c6756417fc8ae1e11896405ddd65718
dywork <dubrovskywork@gmail.com>
2020-11-26 18:04:44 +0500

    переписал main-menu.js на дефолт классы

--- src/components/main-menu/main-menu.js
+++ src/components/main-menu/main-menu.js
@@ -1,72 +1,91 @@
 class MainMenu {
-  constructor(options) {
-    if (options.mainMenu) {
-      this.mainMenu = options.mainMenu;
-      this.mainMenuList = this.mainMenu.querySelectorAll('.main-menu__item');
-      this.mainMenuList.forEach(item => {
-        if (item.classList.contains('main-menu__item_sub-menu')) {
-          item.addEventListener('mouseover', () => {
-            const subMenu = item.querySelector('.main-menu__sub-menu');
-            subMenu.classList.add('main-menu__sub-menu_opened');
-          });
-
-          item.addEventListener('mouseleave', () => {
-            const subMenu = item.querySelector('.main-menu__sub-menu');
-            subMenu.classList.remove('main-menu__sub-menu_opened');
-          });
-        }
-      });
-    } else {
-      console.error('Expected burgerButton(node) inside constructor object but not received');
-    }
-
-    if (options.burgerButton) {
-      this.burgerButton = options.burgerButton;
-      this.burgerButton.addEventListener('click', this.onBurgerButtonClick);
-    } else {
-      console.error('Expected burgerButton(node) inside constructor object but not received');
-    }
-
-    if (options.profileButton) {
-      this.profileButton = options.profileButton;
-      this.profileButton.addEventListener('click', this.onProfileButtonClick);
-    } else {
-      this.profileButton = undefined;
-    }
-
-    this.isProfileActive = false;
-    this.authList = options.authList;
+  constructor(parentDom) {
+    this.parentDom = parentDom;
   }
 
-  onBurgerButtonClick = evt => {
-    evt.preventDefault();
+  init() {
+    this.setSubMenuListener();
+    this.setBurgerListener();
+    this.isAuthorized = !this.parentDom.parentNode.querySelector('.js-profile-btn');
+    if (!this.isAuthorized) {
+      this.setAuthListener();
+    }
+  }
 
-    if (this.isProfileActive) {
-      this.authList.classList.remove('page-header__auth-list_active');
-      this.isProfileActive = false;
+  setSubMenuListener() {
+    const subMenuItems = this.parentDom.querySelectorAll('.js-main-menu-item-sub-menu');
+    subMenuItems.forEach(this.addSubMenuItemListener);
+  }
+
+  setBurgerListener() {
+    const burgerBtn = this.parentDom.querySelector('.js-main-menu-burger-btn');
+    burgerBtn.addEventListener('click', this.handleBurgerBtnClick);
+  }
+
+  handleBurgerBtnClick = () => {
+    this.burgerBtnToggleClass();
+
+    if (!this.isAuthorized) {
+      this.authBtnToggleClass();
+      const authList = this.parentDom.parentNode.querySelector('.js-auth-list');
+      const isAuthListActive = authList.classList.contains('page-header__auth-list_active');
+
+      if (!isAuthListActive) {
+        this.mainMenuToggleClass();
+      }
+
+      if (isAuthListActive) {
+        this.authListToggleClass();
+      }
     } else {
-      this.mainMenu.classList.toggle('main-menu_active');
-    }
-
-    this.burgerButton.classList.toggle('main-menu__burger-icon_active');
-    if (this.profileButton) {
-      this.profileButton.classList.toggle('page-header__auth-profile_hidden');
+      this.mainMenuToggleClass();
     }
   };
 
-  onProfileButtonClick = evt => {
-    evt.preventDefault();
-    if (!this.isProfileActive) {
-      this.authList.classList.add('page-header__auth-list_active');
-      this.burgerButton.classList.add('main-menu__burger-icon_active');
-      this.isProfileActive = true;
-      this.profileButton.classList.add('page-header__auth-profile_hidden');
-    }
+  setAuthListener() {
+    const authBtn = this.parentDom.parentNode.querySelector('.js-profile-btn');
+    authBtn.addEventListener('click', this.handleAuthBtnClick);
+  }
+
+  handleAuthBtnClick = () => {
+    this.burgerBtnToggleClass();
+    this.authBtnToggleClass();
+    this.authListToggleClass();
+  }
+
+  addSubMenuItemListener = subMenuItem => {
+    const subMenuLink = subMenuItem.firstChild;
+    const subMenu = subMenuLink.nextSibling;
+    subMenuItem.addEventListener('mouseover', () => this.subMenuAddClass(subMenu));
+    subMenuItem.addEventListener('mouseleave', () => this.subMenuRemoveClass(subMenu));
   };
 
-  onToggleSubMenu = evt => {
-    evt.preventDefault();
-  };
+  subMenuAddClass(subMenu) {
+    subMenu.classList.add('main-menu__sub-menu_opened');
+  }
+
+  subMenuRemoveClass(subMenu) {
+    subMenu.classList.remove('main-menu__sub-menu_opened');
+  }
+
+  burgerBtnToggleClass() {
+    const burgerBtn = this.parentDom.querySelector('.js-main-menu-burger-btn');
+    burgerBtn.classList.toggle('main-menu__burger-icon_active');
+  }
+
+  mainMenuToggleClass() {
+    this.parentDom.classList.toggle('main-menu_active');
+  }
+
+  authBtnToggleClass() {
+    const authBtn = this.parentDom.parentNode.querySelector('.js-profile-btn');
+    authBtn.classList.toggle('page-header__auth-profile_hidden');
+  }
+
+  authListToggleClass() {
+    const authList = this.parentDom.parentNode.querySelector('.js-auth-list');
+    authList.classList.toggle('page-header__auth-list_active');
+  }
 }
 
 export default MainMenu;
